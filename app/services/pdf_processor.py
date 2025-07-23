5bba949d64f9f8a01554f8eefee46d41ce1a050e
yogeshsyn23 <yogesh.dhanuka@synaptic.com>
2025-07-24 00:57:12 +0530

    vector db integrated

--- app/services/pdf_processor.py
+++ app/services/pdf_processor.py
@@ -1,9 +1,11 @@
 from PyPDF2 import PdfReader
 from app.services.embeddings import embeddings
+from app.services.vector_db import get_chroma_db
 
 class PDFProcessor:
   def __init__(self):
     self.embeddings = embeddings
+    self.chroma_db = get_chroma_db(persistent=True, path="./chroma_db", collection_name="pdf_collection")
 
   def process(self, pdf_path):
     pdf = PdfReader(pdf_path)
@@ -11,4 +13,11 @@ class PDFProcessor:
     print("text: ", text)
     embeddings = self.embeddings.create_embeddings(text)
     print("embeddings: ", embeddings)
+
+    a = self.chroma_db.add(ids=["1"], documents=[text], embeddings=[embeddings])
+    print("a: ", a)
+    query = "What is the main idea of the document?"
+    query_embeddings = self.embeddings.create_embeddings(query)
+    q = self.chroma_db.query(query_embeddings=query_embeddings)
+    print("q: ", q)
     return True
---
+++ app/services/vector_db.py
@@ -0,0 +1,85 @@
+import chromadb
+from chromadb.api.types import QueryResult
+
+class VectorDB:
+
+  _instance = None
+
+  def __init__(self, persistent: bool = False, path: str = None, collection_name: str = "pdf_collection"):
+    try:
+      if persistent:
+        self.client = chromadb.PersistentClient(path=path)
+      else:
+        self.client = chromadb.Client()
+      self.collection = self.client.get_or_create_collection(name=collection_name)
+    except Exception as e:
+      raise e
+   
+
+  @classmethod
+  def get_instance(cls, persistent: bool = False, path: str = None, collection_name: str = "pdf_collection"):
+    """
+    Get the instance of the VectorDB class
+
+    Args:
+      persistent: bool
+        - If True, the database will be persistent and stored in the path
+        - If False, the database will be in memory and will be lost when the program is closed
+      path: str
+        - If persistent is True, the path to the database
+      collection_name: str
+        - The name of the collection to use
+
+    Returns:  
+      VectorDB: Instance of the VectorDB class
+    """
+    if cls._instance is None:
+      cls._instance = cls(persistent=persistent, path=path, collection_name=collection_name)
+    return cls._instance
+
+  def add(self, ids: list[str], documents: list[str], embeddings: list[list[float]]) -> None:
+    """
+    Add documents to the collection
+
+    Args:
+      ids: list[str]
+        - The ids of the documents to add
+      documents: list[str]
+        - The documents to add
+      embeddings: list[list[float]]
+        - The embeddings of the documents to add
+
+    Returns:
+      None
+    """
+    self.collection.add(documents=documents, ids=ids, embeddings=embeddings)
+
+  def query(self, query_embeddings: list[float], n_results: int = 4) -> QueryResult:
+    """
+    Query the collection
+
+    Args:
+      query_embeddings: list[float]
+        - The embeddings of the query
+      n_results: int
+        - The number of results to return
+
+    Returns:
+      QueryResult: The results of the query
+    """
+    return self.collection.query(query_embeddings=[query_embeddings], n_results=n_results)
+
+
+def get_chroma_db(persistent: bool = False, path: str = None, collection_name: str = "pdf_collection") -> VectorDB:
+  """
+  Get the instance of the VectorDB class
+
+  Args:
+    persistent: bool
+    path: str
+    collection_name: str
+
+  Returns:
+    VectorDB: Instance of the VectorDB class
+  """
+  return VectorDB.get_instance(persistent=persistent, path=path, collection_name=collection_name)
