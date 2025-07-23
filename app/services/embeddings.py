d355274e606c9e84919c7cea358d2a7c142b7ffc
yogeshsyn23 <yogesh.dhanuka@synaptic.com>
2025-07-23 23:36:59 +0530

    add method descriptions

--- app/services/embeddings.py
+++ app/services/embeddings.py
@@ -11,16 +11,38 @@ class Embeddings:
   def get_instance(cls) -> 'Embeddings':
     """
     Get singleton instance of the Embeddings class
+
+    Returns:
+      Embeddings: Singleton instance of the Embeddings class
     """
     if cls._instance is None:
       cls._instance = cls()
     return cls._instance
 
   def create_embeddings(self, text: str) -> list[float]:
+    """
+    Create embeddings for the given text
+
+    Args:
+      text: str
+
+    Returns:
+      list[float]: Embeddings for the given text
+    """
     return self.model.encode(text)
   
 
   def similarity(self, text1: str, text2: str) -> float:
+    """
+    Calculate similarity between two texts
+
+    Args:
+      text1: str
+      text2: str
+
+    Returns:
+      float: Similarity between the two texts
+    """
     return self.model.similarity(text1, text2)
 
 embeddings = Embeddings.get_instance()
